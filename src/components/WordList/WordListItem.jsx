5af95011e8d830e0f0ab60edb1708b8ecd6c2426
Oleksandr Khomiak <khomiak@gmail.com>
2023-06-03 12:00:04 +0300

    edit2

--- src/components/WordList/WordListItem.jsx
+++ src/components/WordList/WordListItem.jsx
@@ -2,16 +2,52 @@ import React from 'react';
 
 import Checkbox from '@mui/material/Checkbox';
 
+import { useState } from 'react';
+import { TextField } from '@mui/material';
+
 const WordListItem = ({ index, item, deleteWord, editWord }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [uaWord, setUaWord] = useState(item.uaWord);
+  const [enWord, setEnWord] = useState(item.enWord);
+
+  const handleChange = e => {
+    switch (e.target.name) {
+      case 'uaWord':
+        setUaWord(e.target.value);
+        return;
+      case 'enWord':
+        setEnWord(e.target.value);
+        return;
+      default:
+        return;
+    }
+  };
+
+  const handleEdit = e => {
+    setIsEdit(prevState => !prevState);
+    if (isEdit) {
+      editWord({ ...item, uaWord, enWord });
+    }
+  };
+
   return (
     <div>
       <li>
         <Checkbox />
         <p className="numberWord">{index + 1}</p>
-        <p className="ukrWord">{item.uaWord}</p>
-        <p className="enWord">{item.enWord}</p>
+        {isEdit ? (
+          <TextField value={uaWord} name="uaWord" onChange={handleChange} />
+        ) : (
+          <p className="ukrWord">{item.uaWord} </p>
+        )}
+        {isEdit ? (
+          <TextField value={enWord} name="enWord" onChange={handleChange} />
+        ) : (
+          <p className="enWord">{item.enWord}</p>
+        )}
+
         <button onClick={() => deleteWord(item.id)}>DELETE</button>
-        <button>EDIT</button>
+        <button onClick={handleEdit}>{isEdit ? 'SAVE' : 'EDIT'}</button>
       </li>
     </div>
   );
